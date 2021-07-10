ad6e05488bbf05c662b925b4a0cfdc687f4ab1be
stuti612 <stutiprasad63@gmail.com>
2021-07-10 17:14:30 +0530

    added banner image and fixed some styling

--- src/Home.js
+++ src/Home.js
@@ -5,164 +5,20 @@ import Product from "./Product";
 function Home() {
   return (
     <div className="home">
-      {/* <img
+      <img
         className="home__image"
-        src="https://images.unsplash.com/photo-1528693404014-b13ebe6e723e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1491&q=80"
-      /> */}
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
+        src="https://media.gettyimages.com/photos/farmer-ploughing-field-picture-id1029301814?s=2048x2048"
       />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
-      <Product
-        id="1234"
-        image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
-        name="Yellow Farm Tractor"
-        bPrice={6.6}
-        rPrice={15000}
-      />
+
+      <div className="home__row">
+        <Product
+          id="1234"
+          image="https://images.unsplash.com/photo-1532929900024-6413d2ed39c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
+          name="Yellow Farm Tractor"
+          bPrice={6.6}
+          rPrice={15000}
+        />
+      </div>
     </div>
   );
 }
